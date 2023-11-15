467ea348dafc14369c84fad926c219a4c2b556c8
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-11-15 16:53:26 -0500

    Update AdmissionRequestFactory.kt

--- PMS/src/main/kotlin/seg3x02/pms/domain/division/factory/AdmissionRequestFactory.kt
+++ PMS/src/main/kotlin/seg3x02/pms/domain/division/factory/AdmissionRequestFactory.kt
@@ -1,13 +1,15 @@
 package seg3x02.pms.domain.division.factory
 
 import seg3x02.pms.application.dtos.queries.PatientAdmissionRequestDto
+import seg3x02.pms.application.dtos.queries.PatientAdmissionToDivisionDto
 import seg3x02.pms.domain.division.entities.AdmissionRequest
+import seg3x02.pms.domain.patient.entities.admission.PatientAdmission
 
 /**
  * @author : memo-aldu
  * @mailto : maldu064@uOttawa.ca
  * @created : 11/12/2023, Sunday
  **/
-interface AdmissionRequestFactory {
-    fun createAdmissionRequest(admissionRequestDto: PatientAdmissionRequestDto): AdmissionRequest
+interface AdmissionToDivisionFactory {
+    fun createAdmissionToDivision(admissionToDivisionDto: PatientAdmissionToDivisionDto): PatientAdmission
 }
