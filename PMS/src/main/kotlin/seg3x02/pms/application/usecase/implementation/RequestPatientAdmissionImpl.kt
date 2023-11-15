94461b51bb825f93c9d7197b115232a80ed6dd89
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-11-15 13:48:16 -0500

    Update RequestPatientAdmissionImpl.kt

--- PMS/src/main/kotlin/seg3x02/pms/application/usecase/implementation/RequestPatientAdmissionImpl.kt
+++ PMS/src/main/kotlin/seg3x02/pms/application/usecase/implementation/RequestPatientAdmissionImpl.kt
@@ -4,18 +4,21 @@ import seg3x02.pms.application.dtos.queries.PatientAdmissionRequestDto
 import seg3x02.pms.application.usecase.RequestPatientAdmission
 import seg3x02.pms.domain.division.facade.DivisionFacade
 import seg3x02.pms.domain.patient.facade.PatientFacade
+import java.util.*
 
 class RequestPatientAdmissionImpl(
     private val patientFacade: PatientFacade,
     private val divisionFacade: DivisionFacade
 ) : RequestPatientAdmission {
 
-    override fun requestPatientAdmission(requestDto: PatientAdmissionRequestDto): Boolean {
+    override fun requestPatientAdmission(requestDto: PatientAdmissionRequestDto): UUID? {
         val isPatientAdmitted = patientFacade.isPatientAdmitted(requestDto.patientNAS)
         val doesPatientExist = patientFacade.doesPatientExist(requestDto.patientNAS)
         if (isPatientAdmitted || !doesPatientExist) {
-            return false;
+            return null;
         }
-        return divisionFacade.requestPatientAdmission(requestDto);
+        else
+            divisionFacade.requestPatientAdmission(requestDto);
+        return null;
     }
 }
