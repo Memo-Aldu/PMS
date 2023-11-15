5fcda1a7ead4cab0ec483904f3a089ed6ca0f201
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2023-11-15 12:42:09 -0500

    removed dead code

--- PMS/src/main/kotlin/seg3x02/pms/domain/patient/enums/MaritalStatusEnum.kt
+++ PMS/src/main/kotlin/seg3x02/pms/domain/patient/enums/MaritalStatusEnum.kt
@@ -11,25 +11,4 @@ enum class MaritalStatusEnum {
     DIVORCED,
     WIDOWED,
     SEPARATED;
-
-    fun formInt(value: Int): MaritalStatusEnum {
-        return when (value) {
-            0 -> SINGLE
-            1 -> MARRIED
-            2 -> DIVORCED
-            3 -> WIDOWED
-            4 -> SEPARATED
-            else -> throw IllegalArgumentException("Invalid marital status value")
-        }
-    }
-
-    fun toInt(): Int {
-        return when (this) {
-            SINGLE -> 0
-            MARRIED -> 1
-            DIVORCED -> 2
-            WIDOWED -> 3
-            SEPARATED -> 4
-        }
-    }
 }
--- PMS/src/main/kotlin/seg3x02/pms/domain/patient/enums/PatientKinRelationshipEnum.kt
+++ PMS/src/main/kotlin/seg3x02/pms/domain/patient/enums/PatientKinRelationshipEnum.kt
@@ -13,30 +13,4 @@ enum class PatientKinRelationshipEnum {
     CHILD,
     GUARDIAN,
     OTHER;
-
-
-    fun fromInt(value: Int): PatientKinRelationshipEnum {
-        return when (value) {
-            0 -> MOTHER
-            1 -> FATHER
-            2 -> SIBLING
-            3 -> SPOUSE
-            4 -> CHILD
-            5 -> GUARDIAN
-            6 -> OTHER
-            else -> throw IllegalArgumentException("Invalid patient kin relationship value")
-        }
-    }
-
-    fun toInt(): Int {
-        return when (this) {
-            MOTHER -> 0
-            FATHER -> 1
-            SIBLING -> 2
-            SPOUSE -> 3
-            CHILD -> 4
-            GUARDIAN -> 5
-            OTHER -> 6
-        }
-    }
 }
