b5c0ce85c3809c66502acff4d2191c76f58396ec
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-11-15 12:55:18 -0500

    Update PatientAdmissionRepository.kt

--- PMS/src/main/kotlin/seg3x02/pms/domain/patient/repositories/PatientAdmissionRepository.kt
+++ PMS/src/main/kotlin/seg3x02/pms/domain/patient/repositories/PatientAdmissionRepository.kt
@@ -6,4 +6,7 @@ package seg3x02.pms.domain.patient.repositories
  * @created : 11/12/2023, Sunday
  **/
 interface PatientAdmissionRepository {
+    fun findById(id: String): PatientAdmission?
+    fun finByPatientId(nas: String): PatientAdmission
+    fun save(patientAdmission: PatientAdmission): PatientAdmission
 }
