35c070df0cf4e39d80376450b5cea216be689fe0
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-11-15 16:33:14 -0500

    Update PatientAdmissionToDivisionDto.kt

--- PMS/src/main/kotlin/seg3x02/pms/application/dtos/queries/PatientAdmissionToDivisionDto.kt
+++ PMS/src/main/kotlin/seg3x02/pms/application/dtos/queries/PatientAdmissionToDivisionDto.kt
@@ -7,9 +7,10 @@ package seg3x02.pms.application.dtos.queries
  **/
 data class PatientAdmissionToDivisionDto(
         val patientNAS: String,
+        val divisonId: UUID,
         val requestingChargedNurseNAS: String,
         val approvingChargedNurseNAS: String,
-        val roomNumber: Int,
-        val bedNumber: Int,
+        val roomId: UUID,
+        val bedId: UUID,
         val optionPrivateInsuranceNumber: String?,
 )
