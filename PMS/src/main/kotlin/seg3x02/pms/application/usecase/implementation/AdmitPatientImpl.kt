8d61079318385e08f25c4f82bb8f67c4228718f7
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-11-15 13:49:14 -0500

    Update AdmitPatientImpl.kt

--- PMS/src/main/kotlin/seg3x02/pms/application/usecase/implementation/AdmitPatientImpl.kt
+++ PMS/src/main/kotlin/seg3x02/pms/application/usecase/implementation/AdmitPatientImpl.kt
@@ -4,21 +4,23 @@ import seg3x02.pms.application.dtos.queries.PatientAdmissionToDivisionDto
 import seg3x02.pms.application.usecase.AdmitPatient
 import seg3x02.pms.domain.division.facade.DivisionFacade
 import seg3x02.pms.domain.patient.facade.PatientFacade
+import java.util.*
 
 class AdmitPatientImpl(
     private val patientFacade: PatientFacade,
     private val divisionFacade: DivisionFacade
 ) : AdmitPatient {
 
-    override fun admitPatientToDivision(admissionDto: PatientAdmissionToDivisionDto): Boolean {
+    override fun admitPatientToDivision(admissionDto: PatientAdmissionToDivisionDto): UUID? {
         val availableRoomsAndBeds = divisionFacade.hasAvailableRooms(admissionDto.divisonId)
         val doesPatientExist = patientFacade.isPatientAdmitted(admissionDto.patientNAS)
         val doesDivisionExist = divisionFacade.doesDivisionExist(admissionDto.divisonId)
         if (!availableRoomsAndBeds || !doesPatientExist || !doesDivisionExist) {
-            return false
+            return null
         }
-        divisionFacade.admitPatient(admissionDto)
+        else
+            divisionFacade.admitPatient(admissionDto)
 
-        return true;
+        return null
     }
 }
