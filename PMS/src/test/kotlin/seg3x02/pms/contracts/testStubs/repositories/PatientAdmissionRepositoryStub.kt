46ecfeae90854a3bd2ec4bbfd50196e05c58d5d7
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-11-15 21:40:33 -0500

    Update PatientAdmissionRepositoryStub.kt

--- PMS/src/test/kotlin/seg3x02/pms/contracts/testStubs/repositories/PatientAdmissionRepositoryStub.kt
+++ PMS/src/test/kotlin/seg3x02/pms/contracts/testStubs/repositories/PatientAdmissionRepositoryStub.kt
@@ -16,5 +16,9 @@ class PatientAdmissionRepositoryStub: PatientAdmissionRepository {
     override fun findById(id: UUID): PatientAdmission? {
         return patientAdmissions[id]
     }
+    
+    override fun findByPatientNAS(nas: String): PatientAdmission? {
+        return patientAdmissions.values.find { it.patient.nas == nas }
+    }
 
 }
