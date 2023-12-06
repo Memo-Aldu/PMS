c3da57ba9c993ccf2d02245a0f9768b06287ec02
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-12-06 13:38:28 -0500

    Update ExternalDoctorDtoFactory.kt

--- PMS/src/main/kotlin/seg3x02/pms/adapters/factories/ExternalDoctorDtoFactory.kt
+++ PMS/src/main/kotlin/seg3x02/pms/adapters/factories/ExternalDoctorDtoFactory.kt
@@ -3,6 +3,7 @@ import org.mapstruct.factory.Mappers
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
 import seg3x02.pms.application.dtos.queries.ExternalDoctorDto
+import seg3x02.pms.application.dtos.queries.converters.ExternalDoctorDtoConverter
 import seg3x02.pms.domain.patient.entities.patient.ExternalDoctor
 import seg3x02.pms.domain.patient.factory.ExternalDoctorFactory
 
